7e3de6e29ca9bc4bb5e32287862c646e011e3e3a
waynewei07 <guagua0407@gmail.com>
2023-04-15 21:21:38 +0800

    create Village(Maximum).cpp

--- Temporary Codes/B.cpp
+++ Temporary Codes/B.cpp
@@ -7,64 +7,40 @@ using namespace std;
 #define s second
 #define all(x) x.begin(),x.end()
 #define _ ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pdd pair<long double,long double>
 
 void setIO(string s) {
     freopen((s + ".in").c_str(), "r", stdin);
     freopen((s + ".out").c_str(), "w", stdout);
 }
 
-const int mxn=5e5+5;
-vector<pair<int,int>> adj[mxn];
-int c[mxn];
-bool visited[mxn];
-int n,m;
-
-bool dfs(int v,int k){
-    visited[v]=true;
-    for(auto u:adj[v]){
-        if(u.s<=k) continue;
-        if(visited[u.f]){
-            if(c[u.f]==c[v]) return false;
-            continue;
-        }
-        c[u.f]=1-c[v];
-        if(dfs(u.f,k)==false) return false;
-    }
-    return true;
-}
-
-bool check(int mid){
-    for(int i=1;i<=n;i++){
-        c[i]=0;
-        visited[i]=false;
-    }
-    for(int i=1;i<=n;i++){
-        if(!dfs(i,mid)) return false;
-    }
-    return true;
-}
-
 int main() {_
-    cin>>n>>m;
-    int mx=1;
-    for(int i=0;i<m;i++){
-        int a,b,c;
-        cin>>a>>b>>c;
-        mx=max(mx,c);
-        adj[a].push_back({b,c});
-        adj[b].push_back({a,c});
+    int n;
+    cin>>n;
+    vector<pdd> vec(n);
+    for(int i=0;i<n;i++){
+        cin>>vec[i].f>>vec[i].s;
     }
-    int l=0,r=1e9;
-    while(l<r){
-        int mid=(l+r)/2;
-        if(check(mid)){
-            r=mid;
+    multiset<pdd> S;
+    long double d=1e10;
+    sort(all(vec));
+    int j=0;
+    for(int i=0;i<n;i++){
+        while((vec[i].f-vec[j].f)>=d){
+            S.erase(S.find({vec[j].s,vec[j].f}));
+            j++;
         }
-        else{
-            l=mid+1;
+        auto it=S.lower_bound({vec[i].s-d,-1e9-1});
+        auto bound=S.upper_bound({vec[i].s+d,1e9+1});
+        for(;it!=bound;it++){
+            pdd tmp=*it;
+            //cout<<vec[i].f<<' '<<vec[i].s<<' '<<tmp.f<<' '<<tmp.s<<'\n';
+            d=min(d,sqrt((tmp.s-vec[i].f)*(tmp.s-vec[i].f)+(tmp.f-vec[i].s)*(tmp.f-vec[i].s)));
         }
+        S.insert({vec[i].s,vec[i].f});
     }
-    cout<<l;
+    long double ans=d*d+1e-5;
+    cout<<(ll)(ans);
     return 0;
 }
 //maybe its multiset not set
--- Temporary Codes/C.cpp
+++ Temporary Codes/C.cpp
@@ -13,49 +13,14 @@ void setIO(string s) {
     freopen((s + ".out").c_str(), "w", stdout);
 }
 
-int dx[4]={0,0,1,-1};
-int dy[4]={1,-1,0,0};
-
-void solve(){
-    string str[5];
-    for(int i=0;i<5;i++){
-        for(int j=0;j<7;j++){
-            cin>>str[i][j];
-        }
-    }
-    string ans[5];
-    for(int i=0;i<5;i++){
-        for(int j=0;j<7;j++){
-            ans[i][j]='0';
-        }
-    }
-    for(int i=1;i<5;i++){
-        for(int j=0;j<7;j++){
-            if(str[i-1][j]=='1'){
-                ans[i][j]='1';
-                for(int k=0;k<4;k++){
-                    int x=i+dx[k];
-                    int y=j+dy[k];
-                    if(x>=0 and x<5 and y>=0 and y<7){
-                        if(str[x][y]=='1') str[x][y]='0';
-                        else str[x][y]='1';
-                    }
-                }
-            }
-        }
-    }
-    for(int i=0;i<5;i++){
-        for(int j=0;j<7;j++){
-            cout<<ans[i][j]<<' ';
-        }
-        cout<<'\n';
-    }
-}
+random_device seed;
+mt19937 rng(seed());
 
 int main() {_
-    int t;
-    cin>>t;
-    while(t--) solve();
+    cout<<1000<<'\n';
+    for(int i=0;i<1000;i++){
+        cout<<abs((int)rng())<<' ';
+    }
     return 0;
 }
 //maybe its multiset not set
