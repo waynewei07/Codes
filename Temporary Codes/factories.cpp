ff1b42d8505b218a0a10182bd3e6c5561a915f18
waynewei07 <guagua0407@gmail.com>
2023-04-21 23:46:19 +0800

    create Holiday.cpp

--- Temporary Codes/factories.cpp
+++
@@ -1,8 +0,0 @@
-#include "factories.h"
-
-void Init(int N, int A[], int B[], int D[]) {
-}
-
-long long Query(int S, int X[], int T, int Y[]) {
-  return 0;
-}
--- Temporary Codes/factories.h
+++
@@ -1,7 +0,0 @@
-#ifndef FACTORIES_H_
-#define FACTORIES_H_
-
-void Init(int N, int A[], int B[], int D[]);
-long long Query(int S, int X[], int T, int Y[]);
-
-#endif  /* FACTORIES_H_ */
